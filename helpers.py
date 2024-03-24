f911d5143460390d458c79830a1023bb433ad89d
claireyyu <155686973+claireyyu@users.noreply.github.com>
2024-03-24 12:50:58 -0700

    Update helpers.py to adapt title color to different themes

--- helpers.py
+++ helpers.py
@@ -60,7 +60,7 @@ def display_page_title(title_text: str):
     Parameters:
     - title_text (str): The text for the page title.
     """
-    st.markdown(f"<h1 style='text-align: center; color: black;'>{title_text}</h1>", unsafe_allow_html=True)
+    st.markdown(f"<h1 style='text-align: center;'>{title_text}</h1>", unsafe_allow_html=True)
     st.write("---")
 
 
